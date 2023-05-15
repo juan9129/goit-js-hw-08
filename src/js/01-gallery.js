2d1d5f759cf1ebc8ec2faca005698ae10e5fbbce
Juan D Adames <juandadames@gmail.com>
2023-05-15 16:39:26 -0500

    archivos

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryGrid = galleryItems.map((element) => 
+`<li class= "gallery__item">
+<a class= "gallery__link" href = "${element.original}">
+<img 
+class= "gallery__image"
+src= "${element.preview}"
+alt= "${element.description}"
+/>
+</a>
+</li>`
+)
+.join("");
+
+galleryList.insertAdjacentHTML("afterbegin" , galleryGrid);
+
+    let lightBox = new SimpleLightbox('.gallery .gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250
+    });
