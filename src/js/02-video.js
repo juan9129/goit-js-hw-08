f6500c42a50d4d18370d62521237acda891426e4
Juan D Adames <juandadames@gmail.com>
2023-05-19 16:06:03 -0500

    punto 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from "@vimeo/player";
+
+import {throttle } from "lodash";
+const video = document.querySelector('iframe');
+const player = new Vimeo(video);
+const played = async()=>{
+    try{
+        const time = await player.getCurrentTime();
+        localStorage.setItem('current-time' , time);
+    } 
+    catch(error){
+        console.error("el reporductor ha fallado" , error);
+    }
+};
+
+player.on('play', throttle(played,1000
+));
+
+const currentTime = localStorage.getItem("current-time");
+currentTime ? player.setCurrentTime(currentTime): null;
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import { get, throttle } from "lodash";
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector("input[name="email"]");
+const messageInput = form.querySelector("input[name="message"]");
+
+const saveValueData = () => {
+    const dataForm = {
+        email:emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringfy(dataForm));
+}
+
+const loadValueData = () => {
+    const savedData = localStorage.getItem("feedback-form-state");
+    const {email, message} = savedData ? JSON.parse(savedData) : null;
+    emailInput.value = email;
+    messageInput.value = message;
+}
+
+const sendButton = (Event) => {
+    Event.preventDefault();
+    const dataForm = {
+        email:emailInput.value,
+        message: messageInput.value,
+    };
+}
+
 
