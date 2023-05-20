f2b299baaaf54d849a04db0a2f0ea4b3bee686be
Juan D Adames <juandadames@gmail.com>
2023-05-20 10:59:16 -0500

    punto 3 mal

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,41 @@
-import { get, throttle } from "lodash";
+import { throttle } from 'lodash';
 
-const form = document.querySelector(".feedback-form");
-const emailInput = form.querySelector("input[name="email"]");
-const messageInput = form.querySelector("input[name="message"]");
+const form = document.querySelector('.feedback-form');
 
-const saveValueData = () => {
-    const dataForm = {
-        email:emailInput.value,
-        message: messageInput.value,
-    };
-    localStorage.setItem("feedback-form-state", JSON.stringfy(dataForm));
-}
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
 
-const loadValueData = () => {
-    const savedData = localStorage.getItem("feedback-form-state");
-    const {email, message} = savedData ? JSON.parse(savedData) : null;
-    emailInput.value = email;
-    messageInput.value = message;
-}
+const save_FormData = () => {
+	const form_Data = {
+		email: emailInput.value,
+		message: messageInput.value,
+	};
+	localStorage.setItem('feedback-form-state', JSON.stringify(form_Data));
+};
 
-const sendButton = (Event) => {
-    Event.preventDefault();
-    const dataForm = {
-        email:emailInput.value,
-        message: messageInput.value,
-    };
-}
+const load_FormData = () => {
+	const savedData = localStorage.getItem('feedback-form-state');
+	const { email, message } = savedData ? JSON.parse(savedData) : {};
+	emailInput.value = email;
+	messageInput.value = message;
+};
 
+const handleSubmit = event => {
+	event.preventDefault();
 
+	const form_Data = {
+		email: emailInput.value,
+		message: messageInput.value,
+	};
+
+	localStorage.removeItem('feedback-form-state');
+
+	emailInput.value = '';
+	messageInput.value = '';
+
+	console.log(form_Data);
+};
+
+form.addEventListener('input', throttle(save_FormData, 500));
+window.addEventListener('DOMContentLoaded', load_FormData);
+form.addEventListener('submit', handleSubmit);
