cd4475ecba8792f4b737c1fda5a13a6fdd78dc96
Juan D Adames <juandadames@gmail.com>
2023-06-05 16:36:28 -0500

    punto 2 tarea 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,4 @@
 import SimpleLightbox from "simplelightbox";
-// Importación adicional de estilos
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,19 @@
-import Vimeo from "@vimeo/player";
+import Vimeo from '@vimeo/player';
+import { throttle } from 'lodash';
 
-import {throttle } from "lodash";
-const video = document.querySelector('iframe');
-const player = new Vimeo(video);
-const played = async()=>{
-    try{
-        const time = await player.getCurrentTime();
-        localStorage.setItem('current-time' , time);
-    } 
-    catch(error){
-        console.error("el reporductor ha fallado" , error);
-    }
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const asynchronous = async () => {
+	try {
+		const time_Seconds = await player.getCurrentTime();
+		localStorage.setItem('current-time', time_Seconds);
+	} catch (error) {
+		console.error('Error getting the current playing time of the video', error);
+	}
 };
 
-player.on('play', throttle(played,1000
-));
+player.on('play', throttle(asynchronous, 1000));
 
-const currentTime = localStorage.getItem("current-time");
-currentTime ? player.setCurrentTime(currentTime): null;
-
-
+const current_Time = localStorage.getItem('current-time');
+current_Time ? player.setCurrentTime(current_Time) : null;
