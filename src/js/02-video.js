fabba9663c350791d6edbe55118551fe31577d27
Juan D Adames <juandadames@gmail.com>
2023-06-06 15:29:24 -0500

    tarea 8 punto dos correccion

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,15 @@ import { throttle } from 'lodash';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-const asynchronous = async () => {
+const asynchronous = async data => {
 	try {
-		const time_Seconds = await player.getCurrentTime();
-		localStorage.setItem('current-time', time_Seconds);
+		localStorage.setItem('videoplayer-current-time', data.seconds);
 	} catch (error) {
 		console.error('Error getting the current playing time of the video', error);
 	}
 };
 
-player.on('play', throttle(asynchronous, 1000));
+player.on('timeupdate', throttle(asynchronous, 1000));
 
-const current_Time = localStorage.getItem('current-time');
+const current_Time = localStorage.getItem('videoplayer-current-time');
 current_Time ? player.setCurrentTime(current_Time) : null;
